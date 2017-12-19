af512454607a70923e577ee3e69365c6794eca06
milad asghari <mili@milads-air.home>
2017-12-19 09:12:48 +0330

    test

--- src/components/AddNewContact.js
+++ src/components/AddNewContact.js
@@ -62,3 +62,33 @@ export default class AddNewContact extends Component {
     )
   }
 }
+/*
+render() {
+  const navigation = this.props.navigation;
+  return (
+    <View>
+      <TextInput
+        style={{height: 40, borderWidth: 1}}
+        onChangeText={(text) => this.setState({name:text})}
+        value={this.state.name}
+       />
+       <TextInput
+         style={{height: 40, borderWidth: 1}}
+         onChangeText={(text) => this.setState({email:text})}
+         value={this.state.email}
+       />
+       <TextInput
+         style={{height: 40, borderWidth: 1}}
+         onChangeText={(text) => this.setState({brief:text})}
+         value={this.state.brief}
+        />
+        <Button
+          onPress={()=>this.writeUserData()}
+          title="add"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+   </View>
+  )
+}
+}
